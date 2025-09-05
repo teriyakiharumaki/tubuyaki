12ae55280f645a2752032486b8831d69bfb8f77f
徳永直哉 <ieyasu1118@gmail.com>
2025-09-05 21:02:13 +0900

    フラッシュメッセージを表示されるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "サインアップしました！"
+      redirect_to root_path, notice: "アカウントを作成しました！"
     else
+      flash.now[:alert] = "登録に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
