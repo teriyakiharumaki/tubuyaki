2f777d392d3945ef10d63f3c8a6b523c67fd6b7a
徳永直哉 <ieyasu1118@gmail.com>
2025-09-10 14:30:01 +0900

    ログイン後の画面をポスト一覧に設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "ログインに成功しました！"
+      redirect_to posts_path, notice: "ログインに成功しました！"
     else
       flash.now[:alert] = "メールアドレスまたはパスワードが間違えています"
       render :new, status: :unprocessable_entity
